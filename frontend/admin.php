ad8d07956601a889eeedda2b70fe63b9e0f5ede1
toyapinnock <toyapinnock@gmail.com>
2020-05-19 09:49:56 -0500

    update admin

--- frontend/admin.php
+++ frontend/admin.php
@@ -9,75 +9,129 @@ if($_SESSION['isLogged'] === FALSE){
 <!DOCTYPE html>
 <!--  This site was created in Webflow. http://www.webflow.com  -->
 <!--  Last Published: Sun Nov 24 2019 22:40:25 GMT+0000 (Coordinated Universal Time)  -->
-<html data-wf-page="5ddad8cf05da58f684a25ef0" data-wf-site="5dd89354578babc32818ce3f">
+<html data-wf-page="5dd89354578bab02bf18ce40" data-wf-site="5dd89354578babc32818ce3f">
 <head>
-  <meta charset="utf-8">
-  <title>Admin</title>
-  <script
-  src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous"></script>
-  <script src="azph_hms.js" type="text/javascript"></script>
-  <meta content="Admin" property="og:title">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
-  <meta content="Webflow" name="generator">
-  <link href="css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="css/az-preston-hall-management-system.webflow.css" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">WebFont.load({  google: {    families: ["Roboto:100,300,300italic,regular,500,700,900"]  }});</script>
-  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
-  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
-  <link href="images/webclip.png" rel="apple-touch-icon">
+    <meta charset="utf-8">
+    <title>Preston Hall Management System</title>
+    <meta content="Admin" property="og:title">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+
+
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" herf="/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
-<body>
-  <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
-    <div class="w-container">
-      <nav role="navigation" class="w-nav-menu"><a href="admin.php" class="nav-link w-nav-link">Home</a><a href="index.php" class="nav-link w-nav-link">Sign Out</a>
-      <div class="w-nav-button">
-        <div class="w-icon-nav-menu"></div>
-      </div>
+<body class=" text-center">
+<!--NAV BAR-->
+<section>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg fixed-top  navbar-dark bg-dark">
+            <span class="navbar-brand mb-0 h1">AZPrestonHallManagement</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="confirmation.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Logout <span class="sr-only">(current)</span></a>
+                </li>
+            </div>
+        </nav>
     </div>
-  </div>
-  <div class="hero-section">
-    <div class="w-row">
-      <div class="w-col w-col-6"><img src="images/reading-2.png" srcset="images/reading-2-p-500.png 500w, images/reading-2.png 587w" sizes="(max-width: 479px) 67vw, (max-width: 767px) 587px, (max-width: 804px) 73vw, 41vw" alt=""></div>
-      <div class="w-col w-col-6">
-        <div class="hero-card">
-          <h1>Az Preston Hall Maintenance System</h1>
-          <h3>&gt;&gt; General Updates</h3>
-          <h5>&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore...”</h5><a href="#" class="btn-outline w-button">Button Text</a></div>
-      </div>
+</section>
+<section class="jumbotron" >
+        <div class="container" style="padding:50px; ">
+        <div class="row justify-content-md-center" >
+            <div  >
+                <div class="card" style="width: 85%; height:30rem; padding:30px; box-shadow: 0 4px 8px 0 rgb(0, 89, 255);">
+                <div class="card-body" style="padding:60px;">
+                <div>
+                <h1 class="card-title">AZ Preston Hall Maintenance System</h3>
+
+                <h2 class="card-subtitle mb-2 text-muted">"Pursuing Excellence"</h2>
+
+
+            </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="section">
-    <div class="function-section">
-      <div class="functions">
-        <a href="view-all-issues.php" class="function-card w-inline-block">
-          <h3 class="white funcard">View All Issues</h3>
-        </a>
-        <a href="add-user.php" class="function-card w-inline-block">
-          <h3 class="white funcard">Add User</h3>
-        </a>
-        <a href="update-issue.php" class="function-card w-inline-block">
-          <h3 class="white funcard">Update issue Progress</h3>
-        </a>
-        <a href="view-feedback.html" class="function-card w-inline-block">
-          <h3 class="white funcard">View Feedback</h3>
-        </a>
-        <a href="#" class="function-card w-inline-block">
-          <h3 class="white funcard">Schedule<br>Maintenance</h3>
-        </a>
-        <a href="#" class="function-card w-inline-block">
-          <h3 class="white funcard">Generate Reports</h3>
-        </a>
-      </div>
-      </div>
+</div>
+</div>
+
+</section>
+
+<section class="container-fluid">
+
+    <div  class="row justify-content-md-center" >
+        <div class=" " style="padding:20px;">
+            <div class="card" style="width: 18rem;">
+                <img src="https://media.origaudiopromo.com/EPOIVw-UfdyO1LA/w:300/h:219/q:75/https://origaudiopromo.com/wp-content/uploads/2019/07/space-background-website.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <a href="add-user.php" id="log-issue" >
+                    <h3 class="card-text">Add<br> User</h3>
+                </a>
+
+            </div>
+            </div>
+        </div>
+        <div class="" style="padding:20px;">
+            <div class="card" style="width: 18rem;">
+                <img src="https://media.origaudiopromo.com/EPOIVw-UfdyO1LA/w:300/h:219/q:75/https://origaudiopromo.com/wp-content/uploads/2019/07/space-background-website.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <a  href=" view-all-issues.php "id="track-issue" >
+                    <h3 class="card-text">View All<br> Issues</h3>
+                </a>
+
+            </div>
+            </div>
+        </div>
+        <div class="" style="padding:20px;">
+            <div class="card" style="width: 18rem;">
+                <img src="https://media.origaudiopromo.com/EPOIVw-UfdyO1LA/w:300/h:219/q:75/https://origaudiopromo.com/wp-content/uploads/2019/07/space-background-website.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <a href="update-issue.php" >
+                    <h3 class="card-text">Update Issue<br>Status</h3>
+                </a>
+            </div>
+            </div>
+        </div>
+        <div class="" style="padding:20px;">
+            <div class="card" style="width: 18rem;">
+                <img src="https://media.origaudiopromo.com/EPOIVw-UfdyO1LA/w:300/h:219/q:75/https://origaudiopromo.com/wp-content/uploads/2019/07/space-background-website.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <a href="view-feedback.html">
+                    <h3 class="card-text">View  <br>Feedback</h3>
+                </a>
+            </div>
+            </div>
+        </div>
+        <div class="" style="padding:20px;">
+            <div class="card" style="width: 18rem;">
+                <img src="https://media.origaudiopromo.com/EPOIVw-UfdyO1LA/w:300/h:219/q:75/https://origaudiopromo.com/wp-content/uploads/2019/07/space-background-website.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <a href="">
+                    <h3 class="card-text">Schedule<br> Maintenance</h3>
+                </a>
+            </div>
+            </div>
+
+
     </div>
-  </div>
-  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script src="js/webflow.js" type="text/javascript"></script>
-  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</section>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="app.js" type="text/javascript"></script>
+</body>
+</html>
+
+
 </body>
 </html>
--- frontend/update-issue.php
+++ frontend/update-issue.php
@@ -19,58 +19,70 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <!--  Last Published: Wed Nov 27 2019 21:22:00 GMT+0000 (Coordinated Universal Time)  -->
 <html data-wf-page="5ddee7b3abcf2c65a5f4ee54" data-wf-site="5dd89354578babc32818ce3f">
 <head>
-  <meta charset="utf-8">
-  <title>update issue</title>
-  <script
-  src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous"></script>
-  <script src="azph_hms.js" type="text/javascript"></script>
-  <meta content="update issue" property="og:title">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
-  <meta content="Webflow" name="generator">
-  <link href="css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="css/az-preston-hall-management-system.webflow.css" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">WebFont.load({  google: {    families: ["Roboto:100,300,300italic,regular,500,700,900"]  }});</script>
-  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
-  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
-  <link href="images/webclip.png" rel="apple-touch-icon">
+    <meta charset="utf-8">
+    <title>Preston Hall Management System</title>
+    <meta content="Admin" property="og:title">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+
+
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" herf="/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
-<body>
-  <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
-    <div class="w-container">
-      <nav role="navigation" class="w-nav-menu"><a href="#" class="navbtn w-button">Notifications</a></nav><a href="index.php" class="nav-link w-nav-link">Sign Out</a>
-      <div class="w-nav-button">
-        <div class="w-icon-nav-menu"></div>
-      </div>
-    </div>
-  </div>
-  <div class="hero-section">
-    <div class="form-columns w-row">
-      <div class="w-col w-col-6">
-        <div class="form-card">
-          <h2>Az Preston Hall Maintenance System</h2>
-          <h5>Update Issue Status</h5>
-          <div class="form w-form">
-            <form id="email-form" name="email-form" data-name="Email Form" class="w-clearfix"><label for="name">ID number</label><input type="text" class="w-input" maxlength="256" name="ID-number" data-name="ID number" placeholder="Issue ID" id="ID-number-update" required=""><label for="name">Issue Status</label><select id="current-status" name="cluster" required="" data-name="cluster" class="w-select"><option value="PENDING">Pending</option><option value="FIXING">Fixing</option><option value="FOLLOW UP">Follow up</option><option value="RESOLVED">Resolved</option></select>
-              <h6>All Fields are Mandatory</h6><input id="submit-update-issue" type="submit" value="Submit" data-wait="Please wait..." class="btn-filled blue w-button"></form>
-            <div class="w-form-done">
-              <div>Thank you! Your submission has been received!</div>
-            </div>
-            <div class="w-form-fail">
-              <div>Oops! Something went wrong while submitting the form.</div>
-            </div>
-          </div>
+
+<body class="jumbotron text-center">
+    <!--NAV BAR-->
+    <section>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg fixed-top  navbar-dark bg-dark">
+                <span class="navbar-brand mb-0 h1">AZPrestonHallManagement</span>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="admin.php">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                </div>
+            </nav>
         </div>
-      </div>
-      <div class="w-col w-col-6"><img src="images/status.png" srcset="images/status.png 500w, images/status.png 800w, images/status.png 1080w, images/status.png 1250w" sizes="(max-width: 479px) 92vw, (max-width: 767px) 87vw, (max-width: 991px) 41vw, (max-width: 2906px) 43vw, 1250px" alt=""></div>
-    </div>
-  </div>
-  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script src="js/webflow.js" type="text/javascript"></script>
-  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+    </section>
+    <section class="jumbotron" class="log-in-form">
+        <div class="container" style="padding:20px; ">
+            <div class="row justify-content-md-center">
+                <div class="card" style="width: 60%; height:20rem; padding:20px; box-shadow: 0 4px 8px 0 rgb(0, 89, 255);">
+                    <div id="loginform" class="card-body ">
+                        <h3 class="h3 mb-3 font-weight-normal">AZ Preston HallManagementSystem</h3>
+                        <h5>Update Issue Status</h5>
+                    <form  class="form-signin"id="email-form" name="email-form" data-name="Email Form" class="w-clearfix">
+                            <label for="name" class="sr-only">ID number</label>
+                            <input type="text"class="form-control" maxlength="256" name="ID-number" data-name="ID number" placeholder="Issue ID" id="ID-number-update" required="">
+                            <label for="name" class="sr-only">Issue Status</label>
+                            <select id="current-status" name="cluster" required="" data-name="cluster" class="form-control">
+                                <option value="PENDING">Pending</option>
+                                <option value="FIXING">Fixing</option>
+                                <option value="FOLLOW UP">Follow up</option>
+                                <option value="RESOLVED">Resolved</option>
+                            </select>
+                        <h6>All Fields are Mandatory</h6>
+                        <input id="submit-update-issue" type="submit" value="Submit" data-wait="Please wait..." class="btn btn-lg btn-primary btn-block">
+                    </form>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="app.js" type="text/javascript"></script>
+
 </body>
 </html>
